77a47788a409774d180c36aa2833ed64f4454235
Noedig <163137393+Noe-dig@users.noreply.github.com>
2025-05-14 13:35:08 +0200

    commit

---
+++ SQL/02/01 huisdierendata.sql
@@ -0,0 +1,16 @@
+    INSERT INTO `m4prog`.`huisdierendata`
+(`naam`,
+`soort`,
+`ras`,
+`leeftijd`,
+`geboortedatum`,
+`ingeent`,
+`eigenaar 1`,
+`eigenaar 2`)
+VALUES
+    ('Shadow','Hond','Duitse/Mechelse herder','4','2021-01-01','1','huize Gideon',NULL),
+    ('Simba','Hond','Labrador/Mechelse herder','4','2021-01-01','1','huize Gideon',NULL),
+    ('Roel','Stier','Kleine stier', '2', '2023-01-01','1','huize Gideon',NULL),
+    ('Binkie','Paard','Shetlander','16','2009-01-01','1','huize Gideon',NULL),
+    ('Cthulhu','Great Old One','Kosmisch Wezen','0','0','0','H.P. Lovecraft',NULL),
+    ('Timmy','Kat','Siepert','9','2026-01-01','1','huize Gideon',NULL);
